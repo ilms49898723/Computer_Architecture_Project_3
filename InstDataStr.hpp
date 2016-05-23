12343c6135aa4c1651a55b550aa2e4cdee17920d
LittleBird <ilms49898723@gmail.com>
2016-05-24 01:07:56 +0900

    add comment

--- InstDataStr.hpp
+++ InstDataStr.hpp
@@ -19,38 +19,103 @@ namespace inst {
  */
 class InstDataStr {
 public:
+    /**
+     * Default Constructor
+     */
     InstDataStr();
 
+    /**
+     * Default Destructor
+     */
     virtual ~InstDataStr();
 
+    /**
+     * Get InstType
+     */
     InstType getInstType() const;
 
+    /**
+     * Get OpCode
+     */
     std::string getOpCode() const;
 
+    /**
+     * Get Rs
+     */
     std::string getRs() const;
 
+    /**
+     * Get Rt
+     */
     std::string getRt() const;
 
+    /**
+     * Get Rd
+     */
     std::string getRd() const;
 
+    /**
+     * Get C
+     */
     std::string getC() const;
 
+    /**
+     * Get Funct
+     */
     std::string getFunct() const;
 
+    /**
+     * Set InstType
+     *
+     * @param val InstType to set
+     */
     void setInstType(const InstType& val);
 
+    /**
+     * Set OpCode
+     *
+     * @param val OpCode to set
+     */
     void setOpCode(const std::string& val);
 
+    /**
+     * Set Rs
+     *
+     * @param val Rs to set
+     */
     void setRs(const std::string& val);
 
+    /**
+     * Set Rt
+     *
+     * @param val Rt to set
+     */
     void setRt(const std::string& val);
 
+    /**
+     * Set Rd
+     *
+     * @param val Rd to set
+     */
     void setRd(const std::string& val);
 
+    /**
+     * Set C
+     *
+     * @param val C to set
+     */
     void setC(const std::string& val);
 
+    /**
+     * Set Funct
+     *
+     * @param val Funct to set
+     */
     void setFunct(const std::string& val);
 
+    /**
+     * Convert this to std::string
+     */
     std::string toString() const;
 
 private:
