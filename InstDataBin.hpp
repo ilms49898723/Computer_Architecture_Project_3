e41a7961dadce8062a42bfae43f491849ce3698b
LittleBird <ilms49898723@gmail.com>
2016-05-24 01:05:06 +0900

    update comment

--- InstDataBin.hpp
+++ InstDataBin.hpp
@@ -21,52 +21,146 @@ namespace inst {
  */
 class InstDataBin {
 public:
+    /**
+     * Default constructor
+     */
     InstDataBin();
 
+    /**
+     * Default Destructor
+     */
     virtual ~InstDataBin();
 
+    /**
+     * Get InstType
+     */
     InstType getInstType() const;
 
+    /**
+     * Get OpCode
+     */
     unsigned getOpCode() const;
 
+    /**
+     * Get Rs
+     */
     unsigned getRs() const;
 
+    /**
+     * Get Rt
+     */
     unsigned getRt() const;
 
+    /**
+     * Get Rd
+     */
     unsigned getRd() const;
 
+    /**
+     * Get C
+     */
     unsigned getC() const;
 
+    /**
+     * Get Funct
+     */
     unsigned getFunct() const;
 
+    /**
+     * Get Inst
+     */
     unsigned getInst() const;
 
+    /**
+     * Get RegRead
+     */
     const std::vector<InstElement>& getRegRead() const;
 
+    /**
+     * Get RegWrite
+     */
     const std::vector<InstElement>& getRegWrite() const;
 
+    /**
+     * Get InstName
+     */
     std::string getInstName() const;
 
+    /**
+     * Set InstType
+     *
+     * @param val InstType to set
+     */
     void setInstType(const InstType& val);
 
+    /**
+     * Set OpCode
+     *
+     * @param val OpCode to set
+     */
     void setOpCode(const unsigned& val);
 
+    /**
+     * Set Rs
+     *
+     * @param val Rs to set
+     */
     void setRs(const unsigned& val);
 
+    /**
+     * Set Rt
+     *
+     * @param val Rt to set
+     */
     void setRt(const unsigned& val);
 
+    /**
+     * Set Rd
+     *
+     * @param val Rd to set
+     */
     void setRd(const unsigned& val);
 
+    /**
+     * Set C
+     *
+     * @param val C to set
+     */
     void setC(const unsigned& val);
 
+    /**
+     * Set Funct
+     *
+     * @param val Funct to set
+     */
     void setFunct(const unsigned& val);
 
+    /**
+     * Set Inst
+     *
+     * @param val Inst to set
+     */
     void setInst(const unsigned& val);
 
+    /**
+     * Set InstName
+     *
+     * @param val InstName to set
+     */
     void setInstName(const unsigned& val);
 
+    /**
+     * Set RegRead
+     *
+     * @param reg RegRead to set
+     */
     void setRegRead(const InstElement& reg);
 
+    /**
+     * Set RegWrite
+     *
+     * @param reg RegWrite to set
+     */
     void setRegWrite(const InstElement& reg);
 
 private:
