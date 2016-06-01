f380fd805463c650f4c8db2addc093cf5e7df2e9
LittleBird <ilms49898723@gmail.com>
2016-06-02 00:32:01 +0900

    add basic functions

--- InstPageTable.cpp
+++ InstPageTable.cpp
@@ -12,23 +12,99 @@ namespace inst {
 InstPageTable::InstPageTable() {
     this->pageSize = 0u;
     this->pageEntry = 0u;
+    this->data = nullptr;
+}
+
+InstPageTable::InstPageTable(const InstPageTable& that) {
+    if (this != &that) {
+        if (that.data) {
+            this->pageSize = that.pageSize;
+            this->pageEntry = that.pageEntry;
+            this->data = new PageTableData[this->pageEntry];
+            for (unsigned i = 0; i < this->pageEntry; ++i) {
+                this->data[i] = that.data[i];
+            }
+        }
+        else {
+            this->pageSize = 0;
+            this->pageEntry = 0;
+            this->data = nullptr;
+        }
+    }
+}
+
+InstPageTable::InstPageTable(InstPageTable&& that) {
+    if (this != &that) {
+        this->pageSize = that.pageSize;
+        this->pageEntry = that.pageEntry;
+        this->data = that.data;
+        that.data = nullptr;
+    }
 }
 
 InstPageTable::~InstPageTable() {
-
+    delete[] this->data;
 }
 
 void InstPageTable::init(const unsigned pageSize) {
+    delete[] this->data;
     this->pageSize = pageSize;
     this->pageEntry = 1024u / pageSize;
+    this->data = new PageTableData[this->pageEntry];
+}
+
+void InstPageTable::push(const unsigned tag, const unsigned ppn) {
+    data[tag] = PageTableData(ppn, true);
+}
+
+void InstPageTable::remove(const unsigned tag) {
+    data[tag].valid = false;
+}
+
+std::pair<unsigned, bool> InstPageTable::lookup(const unsigned tag) {
+    if (tag >= pageEntry) {
+        return std::make_pair(0, false);
+    }
+    return std::make_pair(data[tag].ppn, data[tag].valid);
 }
 
 unsigned InstPageTable::size() const {
-    return pageSize;
+    return this->pageSize;
 }
 
 unsigned InstPageTable::entry() const {
-    return pageEntry;
+    return this->pageEntry;
+}
+
+InstPageTable& InstPageTable::operator=(const InstPageTable& that) {
+    if (this != &that) {
+        delete[] this->data;
+        if (that.data) {
+            this->pageSize = that.pageSize;
+            this->pageEntry = that.pageEntry;
+            this->data = new PageTableData[this->pageEntry];
+            for (unsigned i = 0; i < this->pageEntry; ++i) {
+                this->data[i] = that.data[i];
+            }
+        }
+        else {
+            this->pageSize = 0;
+            this->pageEntry = 0;
+            this->data = nullptr;
+        }
+    }
+    return *this;
+}
+
+InstPageTable& InstPageTable::operator=(InstPageTable&& that) {
+    if (this != &that) {
+        delete[] this->data;
+        this->pageSize = that.pageSize;
+        this->pageEntry = that.pageEntry;
+        this->data = that.data;
+        that.data = nullptr;
+    }
+    return *this;
 }
 
 } /* namespace inst */
--- InstPageTable.hpp
+++ InstPageTable.hpp
@@ -8,13 +8,37 @@
 #ifndef COMPUTER_ARCHITECTURE_PROJECT_3_INSTPAGETABLE_HPP_
 #define COMPUTER_ARCHITECTURE_PROJECT_3_INSTPAGETABLE_HPP_
 
+#include <utility>
+
 namespace inst {
 
 class InstPageTable {
+private:
+    struct PageTableData {
+        unsigned ppn;
+        bool valid;
+        PageTableData(const unsigned ppn = 0, const bool valid = false) :
+                ppn(ppn), valid(valid) {}
+    };
+
 public:
     InstPageTable();
 
+    InstPageTable(const InstPageTable& that);
+
+    InstPageTable(InstPageTable&& that);
+
     ~InstPageTable();
+
+    void push(const unsigned tag, const unsigned ppn);
+
+    void remove(const unsigned tag);
+
+    std::pair<unsigned, bool> lookup(const unsigned tag);
+
+    InstPageTable& operator=(const InstPageTable& that);
+
+    InstPageTable& operator=(InstPageTable&& that);
 
 public:
     void init(const unsigned pageSize);
@@ -26,6 +50,7 @@ public:
 private:
     unsigned pageSize;
     unsigned pageEntry;
+    PageTableData* data;
 };
 
 } /* namespace inst */
