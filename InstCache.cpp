cc083bc6a6eac8e177d648fb5a51c1720077a867
LittleBird <ilms49898723@gmail.com>
2016-06-02 19:16:17 +0900

    add basic functions, not tested

--- InstCache.cpp
+++ InstCache.cpp
@@ -10,42 +10,46 @@
 namespace inst {
 
 InstCache::CacheBlock::CacheBlock() {
-    this->content = nullptr;
     this->tag = 0;
+    this->ppn = 0;
+    this->content = nullptr;
+    this->valid = false;
+    this->mru = false;
 }
 
 InstCache::CacheBlock::~CacheBlock() {
     delete[] this->content;
 }
 
-void InstCache::CacheBlock::init(const unsigned size) {
+void InstCache::CacheBlock::allocate(const unsigned size) {
     delete[] this->content;
-    this->content = new unsigned[size];
+    this->content = new unsigned[size >> 2];
+}
+
+void InstCache::CacheBlock::init(const unsigned tag, const unsigned ppn) {
+    this->tag = tag;
+    this->ppn = ppn;
+    this->valid = true;
+    this->mru = true;
 }
 
 InstCache::CacheData::CacheData() {
     this->size = 0;
-    this->valid = nullptr;
-    this->mru = nullptr;
+    this->blockSize = 0;
     this->block = nullptr;
 }
 
 InstCache::CacheData::~CacheData() {
-    delete[] this->valid;
-    delete[] this->mru;
     delete[] this->block;
 }
 
 void InstCache::CacheData::init(const unsigned setAssociativity, const unsigned blockSize) {
-    delete[] this->valid;
-    delete[] this->mru;
     delete[] this->block;
     this->size = setAssociativity;
-    this->valid = new bool[setAssociativity];
-    this->mru = new bool[setAssociativity];
+    this->blockSize = blockSize;
     this->block = new CacheBlock[setAssociativity];
-    for (unsigned i = 0; i < setAssociativity; ++i) {
-        this->block[i].init(blockSize);
+    for (unsigned i = 0; i < this->size; ++i) {
+        this->block[i].allocate(this->blockSize);
     }
 }
 
@@ -55,6 +59,8 @@ InstCache::InstCache() {
     this->setAssociativity = 0;
     this->entry = 0;
     this->data = nullptr;
+    this->hit = 0;
+    this->miss = 0;
 }
 
 InstCache::~InstCache() {
@@ -70,6 +76,77 @@ void InstCache::init(const unsigned cacheSize, const unsigned blockSize, const u
     for (unsigned i = 0; i < this->entry; ++i) {
         this->data[i].init(setAssociativity, blockSize);
     }
+    this->hit = 0;
+    this->miss = 0;
+}
+
+unsigned InstCache::eraseLeastUsed(const unsigned index, InstMemory& memory) {
+    CacheData& targetSet = data[index];
+    unsigned target = 0;
+    for (unsigned i = 0; i < setAssociativity; ++i) {
+        if (targetSet.block[i].valid && !targetSet.block[i].mru) {
+            target = i;
+            break;
+        }
+    }
+    for (unsigned i = 0; i < blockSize; ++i) {
+        memory.setData(targetSet.block[target].ppn, i << 2, targetSet.block[target].content[i]);
+    }
+    targetSet.block[target].valid = false;
+    return target;
+}
+
+bool InstCache::requestBlock(const unsigned physicalAddr, const unsigned ppn) {
+    unsigned tag = getTag(physicalAddr);
+    unsigned index = getIndex(physicalAddr);
+    const CacheData& targetSet = data[index];
+    for (unsigned i = 0; i < setAssociativity; ++i) {
+        if (!targetSet.block[i].valid) {
+            targetSet.block[i].init(tag, ppn);
+            return true;
+        }
+    }
+    return false;
+}
+
+std::pair<unsigned, bool> InstCache::search(const unsigned physicalAddr) {
+    unsigned tag = getTag(physicalAddr);
+    unsigned index = getIndex(physicalAddr);
+    unsigned offset = getOffset(physicalAddr);
+    const CacheData& targetSet = data[index];
+    for (unsigned i = 0; i < setAssociativity; ++i) {
+        if (targetSet.block[i].valid && targetSet.block[i].tag == tag) {
+            ++hit;
+            return std::make_pair(targetSet.block[i].content[offset >> 2], true);
+        }
+    }
+    ++miss;
+    return std::make_pair(0, false);
+}
+
+void InstCache::setData(const unsigned physicalAddr, const unsigned val) {
+    unsigned tag = getTag(physicalAddr);
+    unsigned index = getIndex(physicalAddr);
+    unsigned offset = getOffset(physicalAddr);
+    CacheData& targetSet = data[index];
+    for (unsigned i = 0; i < setAssociativity; ++i) {
+        if (targetSet.block[i].valid && targetSet.block[i].tag == tag) {
+            targetSet.block[i].content[offset >> 2] = val;
+        }
+    }
+}
+
+unsigned InstCache::getData(const unsigned physicalAddr) {
+    unsigned tag = getTag(physicalAddr);
+    unsigned index = getIndex(physicalAddr);
+    unsigned offset = getOffset(physicalAddr);
+    CacheData& targetSet = data[index];
+    for (unsigned i = 0; i < setAssociativity; ++i) {
+        if (targetSet.block[i].valid && targetSet.block[i].tag == tag) {
+            return targetSet.block[i].content[offset >> 2];
+        }
+    }
+    return 0;
 }
 
 unsigned InstCache::getCacheSize() const {
@@ -86,6 +163,26 @@ unsigned InstCache::getSetAssociativity() const {
 
 unsigned InstCache::getEntry() const {
     return this->entry;
+}
+
+unsigned InstCache::getHit() const {
+    return this->hit;
+}
+
+unsigned InstCache::getMiss() const {
+    return this->miss;
+}
+
+unsigned InstCache::getTag(const unsigned physicalAddr) const {
+    return physicalAddr / blockSize / entry;
+}
+
+unsigned InstCache::getIndex(const unsigned physicalAddr) const {
+    return physicalAddr / blockSize % entry;
+}
+
+unsigned InstCache::getOffset(const unsigned physicalAddr) const {
+    return physicalAddr % blockSize;
 }
 
 } /* namespace inst */
--- InstCache.hpp
+++ InstCache.hpp
@@ -8,6 +8,9 @@
 #ifndef COMPUTER_ARCHITECTURE_PROJECT_3_INSTCACHE_HPP_
 #define COMPUTER_ARCHITECTURE_PROJECT_3_INSTCACHE_HPP_
 
+#include <utility>
+#include "InstMemory.hpp"
+
 namespace inst {
 
 class InstCache {
@@ -18,11 +21,16 @@ private:
 
         ~CacheBlock();
 
-        void init(const unsigned size);
+        void allocate(const unsigned size);
+
+        void init(const unsigned tag, const unsigned ppn);
 
     public:
         unsigned tag;
+        unsigned ppn;
         unsigned* content;
+        bool valid;
+        bool mru;
     };
 
     class CacheData {
@@ -35,25 +43,103 @@ private:
 
     public:
         unsigned size;
-        bool* valid;
-        bool* mru;
+        unsigned blockSize;
         CacheBlock* block;
     };
 
 public:
+    /**
+     * Default constructor
+     */
     InstCache();
 
+    /**
+     * Default destructor
+     */
     ~InstCache();
 
+    /**
+     * Initialize
+     *
+     * @param cacheSize cache size
+     * @param blockSize block size
+     * @param setAssociativity cache set associativity
+     */
     void init(const unsigned cacheSize, const unsigned blockSize, const unsigned setAssociativity);
 
+    /**
+     * Erase least recently used block, flush to memory
+     *
+     * @param physicalAddr
+     */
+    unsigned eraseLeastUsed(const unsigned index, InstMemory& memory);
+
+    /**
+     * Request a cache block
+     *
+     * @param physicalAddr physical address
+     * @param ppn physical page number of the address
+     */
+    bool requestBlock(const unsigned physicalAddr, const unsigned ppn);
+
+    /**
+     * Search data in cache at physical address
+     *
+     * @param physicalAddr physical address to search
+     */
+    std::pair<unsigned, bool> search(const unsigned physicalAddr);
+
+    /**
+     * Set value to the block in cache relative to physical address
+     *
+     * @param physicalAddr physical address
+     * @param val value to set
+     */
+    void setData(const unsigned physicalAddr, const unsigned val);
+
+    /**
+     * Get value at the block in cache relative to physical address
+     *
+     * @param physicalAddr physical address
+     */
+    unsigned getData(const unsigned physicalAddr);
+
+    /**
+     * Get cache size in bytes
+     */
     unsigned getCacheSize() const;
 
+    /**
+     * Get cache block size in bytes
+     */
     unsigned getBlockSize() const;
 
+    /**
+     * Get set associativity
+     */
     unsigned getSetAssociativity() const;
 
+    /**
+     * Get number of cache entry
+     */
     unsigned getEntry() const;
+
+    /**
+     * Get hit
+     */
+    unsigned getHit() const;
+
+    /**
+     * Get miss
+     */
+    unsigned getMiss() const;
+
+private:
+    unsigned getTag(const unsigned physicalAddr) const;
+
+    unsigned getIndex(const unsigned physicalAddr) const;
+
+    unsigned getOffset(const unsigned physicalAddr) const;
 
 private:
     unsigned cacheSize;
@@ -61,6 +147,10 @@ private:
     unsigned setAssociativity;
     unsigned entry;
     CacheData* data;
+
+private:
+    unsigned hit;
+    unsigned miss;
 };
 
 } /* namespace inst */
