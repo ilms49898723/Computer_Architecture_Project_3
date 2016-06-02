a77fbc3f6c9150af00ab4d8b4810bdbc8be8652c
LittleBird <ilms49898723@gmail.com>
2016-06-02 11:47:05 +0900

    add basic init functions

--- InstCache.cpp
+++ InstCache.cpp
@@ -9,20 +9,83 @@
 
 namespace inst {
 
+InstCache::CacheBlock::CacheBlock() {
+    this->content = nullptr;
+    this->tag = 0;
+}
+
+InstCache::CacheBlock::~CacheBlock() {
+    delete[] this->content;
+}
+
+void InstCache::CacheBlock::init(const unsigned size) {
+    delete[] this->content;
+    this->content = new unsigned[size];
+}
+
+InstCache::CacheData::CacheData() {
+    this->size = 0;
+    this->valid = nullptr;
+    this->mru = nullptr;
+    this->block = nullptr;
+}
+
+InstCache::CacheData::~CacheData() {
+    delete[] this->valid;
+    delete[] this->mru;
+    delete[] this->block;
+}
+
+void InstCache::CacheData::init(const unsigned setAssociativity, const unsigned blockSize) {
+    delete[] this->valid;
+    delete[] this->mru;
+    delete[] this->block;
+    this->size = setAssociativity;
+    this->valid = new bool[setAssociativity];
+    this->mru = new bool[setAssociativity];
+    this->block = new CacheBlock[setAssociativity];
+    for (unsigned i = 0; i < setAssociativity; ++i) {
+        this->block[i].init(blockSize);
+    }
+}
+
 InstCache::InstCache() {
-    this->cacheSize = 0u;
-    this->blockSize = 0u;
-    this->setAssociativity = 0u;
+    this->cacheSize = 0;
+    this->blockSize = 0;
+    this->setAssociativity = 0;
+    this->entry = 0;
+    this->data = nullptr;
 }
 
 InstCache::~InstCache() {
-
+    delete[] this->data;
 }
 
 void InstCache::init(const unsigned cacheSize, const unsigned blockSize, const unsigned setAssociativity) {
     this->cacheSize = cacheSize;
     this->blockSize = blockSize;
     this->setAssociativity = setAssociativity;
+    this->entry = cacheSize / setAssociativity / blockSize;
+    this->data = new CacheData[this->entry];
+    for (unsigned i = 0; i < this->entry; ++i) {
+        this->data[i].init(setAssociativity, blockSize);
+    }
+}
+
+unsigned InstCache::getCacheSize() const {
+    return this->cacheSize;
+}
+
+unsigned InstCache::getBlockSize() const {
+    return this->blockSize;
+}
+
+unsigned InstCache::getSetAssociativity() const {
+    return this->setAssociativity;
+}
+
+unsigned InstCache::getEntry() const {
+    return this->entry;
 }
 
 } /* namespace inst */
--- InstCache.hpp
+++ InstCache.hpp
@@ -11,18 +11,56 @@
 namespace inst {
 
 class InstCache {
+private:
+    class CacheBlock {
+    public:
+        CacheBlock();
+
+        ~CacheBlock();
+
+        void init(const unsigned size);
+
+    public:
+        unsigned tag;
+        unsigned* content;
+    };
+
+    class CacheData {
+    public:
+        CacheData();
+
+        ~CacheData();
+
+        void init(const unsigned setAssociativity, const unsigned blockSize);
+
+    public:
+        unsigned size;
+        bool* valid;
+        bool* mru;
+        CacheBlock* block;
+    };
+
 public:
     InstCache();
 
     ~InstCache();
 
-public:
     void init(const unsigned cacheSize, const unsigned blockSize, const unsigned setAssociativity);
+
+    unsigned getCacheSize() const;
+
+    unsigned getBlockSize() const;
+
+    unsigned getSetAssociativity() const;
+
+    unsigned getEntry() const;
 
 private:
     unsigned cacheSize;
     unsigned blockSize;
     unsigned setAssociativity;
+    unsigned entry;
+    CacheData* data;
 };
 
 } /* namespace inst */
